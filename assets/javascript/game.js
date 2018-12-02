85e86f3b8545dc00852bb7c3d5ed52a65a5f2403
John Hansberry <johnvhansberry@gmail.com>
2018-12-02 00:58:40 -0500

    updated files 12.1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    //set initial varialbles
+    //set initial varialbles for wins and losses
     var wins = 0
     var losses = 0
     //randomize target and gems
@@ -12,61 +12,88 @@ $(document).ready(function () {
     var currentNumber = 0;
 
     //Reset function after win or loss
-    var reset = function () {
-        var wins = 0
-        var losses = 0
+    function reset() {
+
         //randomize target and gems
-        var targetNumber = Math.floor(Math.random() * 101 + 19);
-        var greenGem = Math.floor(Math.random() * 11 + 1);
-        var pinkGem = Math.floor(Math.random() * 11 + 1);
-        var blueGem = Math.floor(Math.random() * 11 + 1);
-        var yellowGem = Math.floor(Math.random() * 11 + 1);
+        targetNumber = Math.floor(Math.random() * 101 + 19);
+        greenGem = Math.floor(Math.random() * 11 + 1);
+        pinkGem = Math.floor(Math.random() * 11 + 1);
+        blueGem = Math.floor(Math.random() * 11 + 1);
+        yellowGem = Math.floor(Math.random() * 11 + 1);
         //var used for user score
-        var currentNumber = 0;
+        currentNumber = 0;
         //print target score
         $(".target-score").text("Target Score: " + targetNumber);
-
+        //print current user score
+        $(".tally-score").text(currentNumber);
     }
-
+    var wins = function() {
+        wins++;
+        alert("Congratulations! You Win!");
+        $(".wins").text("Wins: " + wins);
+        reset();
+    }
+    var losses = function () {
+        losses++;
+        alert("I'm sorry you lose! Please try again");
+        $(".losses").text("Losses: " + losses);
+        reset();
+    }
     //print target score
     $(".target-score").text("Target Score: " + targetNumber);
+    //print current user score
+    $(".tally-score").text(currentNumber);
 
-    if (currentNumber < targetNumber) {
         //click functions
         $(".green").on("click", function () {
             //value of the gem added to the user score
-            currentNumber += greenGem;
+            currentNumber = currentNumber + greenGem;
             //print updated user score
             $(".tally-score").text(currentNumber);
+            if (currentNumber === targetNumber) {
+                wins();
+            }
+            else if (currentNumber > targetNumber) {
+                losses();
+            }
         })
         $(".pink").on("click", function () {
-            currentNumber += pinkGem;
+            currentNumber = currentNumber + pinkGem;
             $(".tally-score").text(currentNumber);
+            if (currentNumber === targetNumber) {
+                wins();
+            }
+            else if (currentNumber > targetNumber) {
+                losses();
+            }
         })
         $(".blue").on("click", function () {
-            currentNumber += blueGem;
+            currentNumber = currentNumber + blueGem;
             $(".tally-score").text(currentNumber);
+            if (currentNumber === targetNumber) {
+                wins();
+            }
+            else if (currentNumber > targetNumber) {
+                losses();
+            }
         })
         $(".yellow").on("click", function () {
-            currentNumber += yellowGem;
+            currentNumber = currentNumber + yellowGem;
             $(".tally-score").text(currentNumber);
+            if (currentNumber === targetNumber) {
+                wins();
+            }
+            else if (currentNumber > targetNumber) {
+                losses();
+            }
+
         })
-        return (currentNumber);
-    }
+    
+    
+
 
     //else if statement for when user's number is equal to or greater than target
 
-    else if (currentNumber === targetNumber) {
-        wins++;
-        alert("Congratulations! You Win!");
-        $(".wins-and-losses").text("Wins: " + wins);
-        reset()
-    }
-    else if (currentNumber > targetNumber) {
-        losses++;
-        alert("Sorry You Lose! Please try again!");
-        $(".wins-and-losses").text("Losses: " + losses);
-        reset()
-    }
 
+    reset();
 })
